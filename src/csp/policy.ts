ff5c8f92f4a8f593636ce94f14b11953394717cd
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-08-04 22:38:33 +0900

    Accept `null` for arguments of `serializePolicy()`

--- src/csp/policy.ts
+++ src/csp/policy.ts
@@ -1,5 +1,8 @@
+import { isNotNull, type Nullable } from 'option-t/Nullable/Nullable';
+
 // https://w3c.github.io/webappsec-csp/#grammardef-serialized-policy
-export function serializePolicy(...values: Array<string>) {
-    const result = values.join('; ');
+export function serializePolicy(...values: Array<Nullable<string>>) {
+    const nonNull = values.filter(isNotNull);
+    const result = nonNull.join('; ');
     return result;
 }
