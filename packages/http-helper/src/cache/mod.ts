cab5044b2227871fe87f9b41c68dd0b8a9b97196
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-07-22 00:33:21 +0900

    Implement Cache-Control's Request Directives
    - https://www.rfc-editor.org/rfc/rfc9111.html#name-request-directives
    - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#request_directives

---
+++ packages/http-helper/src/cache/mod.ts
@@ -0,0 +1,5 @@
+//  @see
+//  - [RFC 9111 HTTP Caching](https://www.rfc-editor.org/rfc/rfc9111.html)
+//  - [RFC 9213 Targeted HTTP Cache Control](https://www.rfc-editor.org/rfc/rfc9213)
+
+export * as RequestDirective from './request_directive.js';
