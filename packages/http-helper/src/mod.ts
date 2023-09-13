2ecf8aa11718f4fcc1f311a8c5caa34cdd55753f
Ryoma Abe <ryoma.abe@nex.nikkei.com>
2023-09-14 01:14:21 +0900

    Add `HttpMethodType` type that represents all available http methods

--- packages/http-helper/src/mod.ts
+++ packages/http-helper/src/mod.ts
@@ -1,9 +1,12 @@
+import * as HttpMethod from './http_method.js';
+
 export * as Auth from './auth/mod.js';
 export * as Cache from './cache/mod.js';
 export * as ClearSiteData from './clear_site_data.js';
 export * as CSP from './csp/mod.js';
 export * as HttpHeaderName from './http_header_name.js';
-export * as HttpMethod from './http_method.js';
+export { HttpMethod };
+export type HttpMethodType = keyof typeof HttpMethod;
 export * as HttpStatus from './http_status_code.js';
 export * as Mime from './mime.js';
 export { SecFetchDest, SecFetchMode, SecFetchSite, SecFetchUser } from './sec_fetch/mod.js';
